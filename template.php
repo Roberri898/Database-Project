68b107ed33a8c8751435e2e5ea26fd5a2b92b66d
RobinBelo01 <robin.belo01@utrgv.edu>
2018-11-16 20:34:54 -0600

    Fixed the issing<?php

--- template.php
+++ template.php
@@ -4,7 +4,18 @@
 		<meta charset="utf-8">
 		<title>Database Demo</title>
 	</head>
-	
+		<?php
+			$servername = "localhost";
+			$username = "<username>";
+			$password = "<password>";
+			$dbname = "landlord";
+			$conn = new mysqli($servername, $username, $password, $dbname);
+
+			if ($conn->connect_error)
+			    die("Connection failed: " . $conn->connect_error);
+
+			$conn->close();
+		?>
 	<body>
 	</body>
 </html>
--- tenanat.php
+++ tenanat.php
@@ -5,42 +5,43 @@
 		<title>Tenant Page</title>
 
 	</head>
-		$servername = "localhost";
-		$username = "<username>";
-		$password = "<password>";
-		$dbname = "landlord";
-		$conn = new mysqli($servername, $username, $password, $dbname);
+		<?php
+			$servername = "localhost";
+			$username = "<username>";
+			$password = "<password>";
+			$dbname = "landlord";
+			$conn = new mysqli($servername, $username, $password, $dbname);
 
-		if ($conn->connect_error)
-		    die("Connection failed: " . $conn->connect_error);
+			if ($conn->connect_error)
+			    die("Connection failed: " . $conn->connect_error);
 
-		echo "TENANTS<br>";
-			$sql = "SELECT Tenant_ID, Tenant_FirstName,Tenant_LastName,Tenant_Age,Tenant_Gender FROM Tenant";
-			$result = $conn->query($sql);
-			if ($result->num_rows > 0) 
-			{
-			    while($row = $result->fetch_assoc()) 
+			echo "TENANTS<br>";
+				$sql = "SELECT Tenant_ID, Tenant_FirstName,Tenant_LastName,Tenant_Age,Tenant_Gender FROM Tenant";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) 
 				{
-			        echo "id: " . $row["Tenant_ID"].
-						" - Name: " . $row["Tenant_FirstName"]. " " . $row["Tenant_LastName"].
-						" - Age: " . $row["Tenant_Age"]. " ".
-						" - Gender: ";
-						if ($row["Tenant_Gender"] == 0)
-						{
-							echo "Male<br>";
-						}
-						else if ($row["Tenant_Gender"] == 1)
-						{
-							echo "Female<br>";
-						}
-			    }
-				echo "<br>------------------------------------------------<br>";
-			} 
-			else 
-			{
-			    echo "0 results<br>------------------------------------------------<br>";
-			}
-			$conn->close();
+				    while($row = $result->fetch_assoc()) 
+					{
+				        echo "id: " . $row["Tenant_ID"].
+							" - Name: " . $row["Tenant_FirstName"]. " " . $row["Tenant_LastName"].
+							" - Age: " . $row["Tenant_Age"]. " ".
+							" - Gender: ";
+							if ($row["Tenant_Gender"] == 0)
+							{
+								echo "Male<br>";
+							}
+							else if ($row["Tenant_Gender"] == 1)
+							{
+								echo "Female<br>";
+							}
+				    }
+					echo "<br>------------------------------------------------<br>";
+				} 
+				else 
+				{
+				    echo "0 results<br>------------------------------------------------<br>";
+				}
+				$conn->close();
 			?>
 	<body>
 	</body>
